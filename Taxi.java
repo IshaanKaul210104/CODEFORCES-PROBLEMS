9b4cd122762e29bdde96df245d904bd46ffd226a
IshaanKaul210104 <166923075+IshaanKaul210104@users.noreply.github.com>
2026-01-30 23:49:02 +0530

    /

--- Taxi.java
+++ Taxi.java
@@ -6,20 +6,33 @@ public class Taxi {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int[] arr = new int[n];
+        int count1 = 0, count2 = 0, count3 = 0, count4 = 0;
         for(int i = 0;i < n;i++){
-            arr[i] = sc.nextInt();   
+            arr[i] = sc.nextInt(); 
+            if(arr[i] == 1) count1++;
+            else if(arr[i] == 2) count2++;
+            else if(arr[i] == 3) count3++;
+            else if(arr[i] == 4) count4++;
         }
-        Arrays.sort(arr);
-        int currSum = 0, count = 0;
-        for(int i = 0;i < n;i++){
-            if(currSum + arr[i] > 4){
-                count++;
-                currSum = 0;
+        int taxis = count4;
+        while(count3 > 0 && count1 > 0){
+            count3--;
+            count1--;
+            taxis++;
+        }
+        taxis += count3;
+        taxis += count2 / 2;
+        taxis += count2 % 2;
+        if(count2 % 2 == 1){
+            count1 -= 2;
+        }
+        if(count1 > 0){
+            taxis += count1 / 4;
+            if(count1 % 4 != 0){
+                taxis++;
             }
-            currSum += arr[i];
         }
-        if(currSum > 0) count++;
-        System.out.println(count);
+        System.out.println(taxis);
         sc.close();
     }
 }
